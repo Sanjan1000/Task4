2c2839ff9f3af1ead86cbd6e2aa7ac58421ac889
Sanjan1000 <95492587+Sanjan1000@users.noreply.github.com>
2024-04-30 17:02:51 +0600

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,25 +21,19 @@ class UsersController < ApplicationController
   end
   
   def usage
-    unless current_user.is_blocked?
-      return redirect_to new_user_session_path, alert: "You are not authorized to block users."
-    end
-  
     selected_user_ids = params[:user_ids]
   
     if params[:block]
       User.where(id: selected_user_ids).update_all(is_blocked: "blocked")
-      flash[:notice] = "Selected users have been blocked."  # Use flash for success message
+      flash[:notice] = "Selected users have been blocked."  
     elsif params[:delete]
       User.where(id: selected_user_ids).destroy_all
-      flash[:notice] = "Selected users have been deleted."  # Use flash for success message
+      flash[:notice] = "Selected users have been deleted."  
     elsif params[:unblock]
-      User.where(id: selected_user_ists).update_all(is_blocked: "active")
-      flash[:notice] = "Selected users have been unblocked."  # Use flash for success message
+      User.where(id: selected_user_ids).update_all(is_blocked: "active")
+      flash[:notice] = "Selected users have been unblocked." 
     end
-  
-    # Redirect to users_path or a custom path after action
-    if current_user.is_blocked?
+    if current_user.is_blocked=="blocked"
       redirect_to new_user_session_path
     else
       redirect_to users_path
